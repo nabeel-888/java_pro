1ceede65f2a55ed624ee62d3a126c568edfd74e8
nabeel-888 <nabeelansari888@gmail.com>
2023-09-25 18:16:58 +0530

    array prac program value change

--- company/Arrays/array_prac.java
+++ company/Arrays/array_prac.java
@@ -2,7 +2,7 @@ package com.company.Arrays;
 
 public class array_prac {
     public static void main(String[] args) {
-        int arr[] = {33,24,23};
+        int arr[] = {10,11,12};
 
         for (int i=0;i<arr.length;i++){
             System.out.println(arr[i]);
