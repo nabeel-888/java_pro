a6a8109b83e800360c856f042b05f5650227e43e
nabeel-888 <nabeelansari888@gmail.com>
2023-09-25 18:26:07 +0530

    array _pra

--- company/Arrays/array_prac.java
+++ company/Arrays/array_prac.java
@@ -2,7 +2,7 @@ package com.company.Arrays;
 
 public class array_prac {
     public static void main(String[] args) {
-        int arr[] = {10,11,12};
+        int arr[] = {21,22,24};
 
         for (int i=0;i<arr.length;i++){
             System.out.println(arr[i]);
