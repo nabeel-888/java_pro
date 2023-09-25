aaa7bec6e015e2cea711203caa42f4c570611bb3
nabeel-888 <nabeelansari888@gmail.com>
2023-09-25 18:14:04 +0530

    array prac program

---
+++ company/Arrays/array_prac.java
@@ -0,0 +1,11 @@
+package com.company.Arrays;
+
+public class array_prac {
+    public static void main(String[] args) {
+        int arr[] = {33,24,23};
+
+        for (int i=0;i<arr.length;i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
